dd0aa7323775d804b3d0bb41d809e03811d103b2
Marios <touloupos@gmail.com>
2020-07-15 14:22:06 +0300

    Ádd container name in config instead of static ip

--- monitoring_system/config.py
+++ monitoring_system/config.py
@@ -1,5 +1,5 @@
 class monitoring_vars:
-    HOST_DOMAIN = "10.10.70.21"
+    HOST_DOMAIN = "monitoring_system_pushgateway_1"
     PROMETHEUS_PORT = "9090"
     PG_PORT = "9091"
     ALERT_MANAGER = "9093"
