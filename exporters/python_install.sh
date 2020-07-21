7a336ffc24cfaccde72f494dc24e990de716d660
Marios <touloupos@gmail.com>
2020-07-21 12:32:17 +0300

    Add server_info exporter in each node of the network

---
+++ exporters/python_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update
+
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+
+cd /tmp
+
+wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz
+
+tar –xf Python-3.7.5.tgz
+
+cd Python-3.7.5
+
+./configure ––enable–optimizations
+
+make altinstall
+
+make install
+
+apt install -y python3-pip
+
+pip3 install -r server_info/requirements.txt
+
+python3 server_info/server_info.py
