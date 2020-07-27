a1cdcc073bd637c3fcb4ef9b413e8e8fdc53811f
Antonios Inglezakis <inglezakis.a@unic.ac.cy>
2020-07-27 14:06:28 +0300

    Creates a docker network and calls xrpl-unl-manager starting script. 	modified:   run_testnet.sh

--- run_testnet.sh
+++ run_testnet.sh
@@ -10,6 +10,8 @@ VAL_NAME_PREFIX=${VAL_NAME_PREFIX:-"validator-"}
 PEER_PORT=${PEER_PORT:-51235}
 IMAGE_TAG=${IMAGE_TAG:-"v1.5"}
 
+TESTNET_NAME=${TESTNET_NAME:-"xrpl-testnet"}
+
 source scripts/helper_functions.sh
 source scripts/gen_valkeys.sh
 
@@ -18,11 +20,20 @@ VAL_NUM=${1:-0}
 generate_keys_and_configs ${VAL_NUM}
 
 dockercompose_testnet_generator ${VAL_NUM} ${OUTPUT_DIR}
+# Creating Testnet
+docker network create ${TESTNET_NAME}
+
+# Run UNL manager containers
+echo "Running UNL manager containers..."
+
+BASE_DIR=$(pwd)/xrpl-unl-manager ./xrpl-unl-manager/start_UNL_manager_services.sh
+
+echo "    Done!"
 
 #run testnet
 echo "Starting the testnet..."
 
-CONFIGFILES=${OUTPUT_DIR} IMAGE_TAG=${IMAGE_TAG} docker-compose -f ${WORKING_DIR}/${COMPOSE_FILENAME} up -d
+TESTNET_NAME=${TESTNET_NAME} CONFIGFILES=${OUTPUT_DIR} IMAGE_TAG=${IMAGE_TAG} docker-compose -f ${WORKING_DIR}/${COMPOSE_FILENAME} up -d
 
 echo "Waiting for everything goes up..."
 sleep 1
